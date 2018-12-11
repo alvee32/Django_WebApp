5eed89bb66277abdfbe6b2ad784ab1e9655bfcea
alvee32 <alvmz32@gmail.com>
2018-12-12 00:40:15 +0600

    real timestamp

--- tweetme/src/tweets/models.py
+++ tweetme/src/tweets/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Tweet(models.Model):
 	content   = models.CharField(max_length=140)
 	updated = models.DateTimeField(auto_now=True)
-	timestamp = models.DateTimeField(auto_now_add=True) 
+	timestamp = models.DateTimeField(auto_now_add=True)
 
 	
 	def __str__(self):
