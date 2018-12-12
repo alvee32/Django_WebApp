7af23bffa2ca163e85b329d76d2f16bfe913ca5a
alvee32 <alvmz32@gmail.com>
2018-12-13 03:49:37 +0600

    querying database

--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
 
 #retrive
 def tweet_detail_view(request, id=1):
-	return render (request, "tweets/detail_view.html", {})
+	obj = Tweet.objects.get(id=1)
+	print(obj)
+	context = {
+	"objects": obj
+	}
+	return render (request, "tweets/detail_view.html", context)
 
 def tweet_list_view(request):
-	return render (request, "tweets/list_view.html", {})
+	queryset = Tweet.objects.all()
+	print(queryset)
+	for obj in queryset:
+		print(obj.content)
+		context = {
+		"objects_list": queryset
+		}
+	return render (request, "tweets/list_view.html", context)
