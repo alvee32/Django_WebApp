e5b814a58f6c6f2ecedc2d57d7fbcffa6c3a0beb
alvee32 <alvmz32@gmail.com>
2018-12-13 04:02:49 +0600

    queryset in listview

--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -5,10 +5,10 @@ from .models import Tweet
 
 #retrive
 def tweet_detail_view(request, id=1):
-	obj = Tweet.objects.get(id=1)
+	obj = Tweet.objects.get(id=id) #get from database
 	print(obj)
 	context = {
-	"objects": obj
+	"object": obj
 	}
 	return render (request, "tweets/detail_view.html", context)
 
@@ -17,7 +17,7 @@ def tweet_list_view(request):
 	print(queryset)
 	for obj in queryset:
 		print(obj.content)
-		context = {
-		"objects_list": queryset
+	context = {
+		"object_list": queryset
 		}
 	return render (request, "tweets/list_view.html", context)
