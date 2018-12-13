b352a6cd05bbdcf8074588371d8831dd60468aa4
alvee32 <alvmz32@gmail.com>
2018-12-13 22:48:26 +0600

    tweet list view

--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -7,15 +7,21 @@ from .models import Tweet
 #retrive
 
 class TweetDetailView(DetailView):
-	template_name = "tweets/detail_view.html"
+	#template_name = "tweets/detail_view.html"
 	queryset = Tweet.objects.all()
 
 	def get_object(self):
 		return Tweet.objects.get(id=1)
 
 class TweetListView(ListView):
-	template_name = "tweets/list_view.html"
+	#template_name = "tweets/list_view.html"
 	queryset = Tweet.objects.all()
+
+	def get_context_data(self, *args, **kwargs):
+		context = super (TweetListView, self).get_context_data(*args, **kwargs)
+		#context["another_list"] = Tweet.objects.all()
+		#print(context)
+		return context
 		
 		
 
