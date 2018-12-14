6924915c5f23e627402b6e1fb971f05179461709
alvee32 <alvmz32@gmail.com>
2018-12-15 02:25:58 +0600

    user check

--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -6,10 +6,11 @@ from .models import Tweet
 #create
 
 class TweetCreateView(CreateView):
-	queryset = Tweet.objects.all()
 	form_class = TweetModelForm
 	template_name = 'tweets/create_view.html'
-	#fields = ['user', 'content']
+    def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super (TweetCreateView, self).form_valid(form)
 
 #update
 
