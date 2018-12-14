b0020a960193bccbbb204cc47113c69ea7638ae1
alvee32 <alvmz32@gmail.com>
2018-12-14 18:17:17 +0600

    dynamic work

--- tweetme/src/tweets/urls.py
+++ tweetme/src/tweets/urls.py
@@ -5,8 +5,8 @@ from .views import TweetListView, TweetDetailView
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', TweetListView.as_view(), name='list'),
-    url(r'^1/$', TweetDetailView.as_view(), name='detail'),
+    url(r'^$', TweetListView.as_view(), name='list'), #tweet/
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), #tweet/1/
 
 ]
 
--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-
 from .models import Tweet
 # Create your views here.
 
@@ -10,8 +9,12 @@ class TweetDetailView(DetailView):
 	#template_name = "tweets/detail_view.html"
 	queryset = Tweet.objects.all()
 
-	def get_object(self):
-		return Tweet.objects.get(id=1)
+	# def get_object(self):
+	#	print (self.kwargs)
+	#	pk = self.kwargs.get("pk")
+	#	print(pk)
+	#	return Tweet.objects.get(id=pk)
+
 
 class TweetListView(ListView):
 	#template_name = "tweets/list_view.html"
