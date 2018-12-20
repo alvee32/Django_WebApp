73acbae980b5cea5edc0f7dccc352983a9567e5c
maliha17 <mehnazlasmi213@gmail.com>
2018-12-20 21:24:52 +0600

    Create view

--- tweetme/src/tweets/views.py
+++ tweetme/src/tweets/views.py
@@ -14,11 +14,10 @@ from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
 
-class TweetCreateView(FormUserNeededMixin, CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView);
 	form_class = TweetModelForm
 	template_name = 'tweets/create_view.html'
 	#success_url = reverse_lazy("tweet:detail")
-
 
 class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
 	queryset = Tweet.objects.all()
