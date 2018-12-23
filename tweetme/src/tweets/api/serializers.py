e53befd9e46653e3f54fc704742abac947d68c34
alvee32 <alvmz32@gmail.com>
2018-12-23 22:22:17 +0600

    retweet function

--- tweetme/src/tweets/api/serializers.py
+++ tweetme/src/tweets/api/serializers.py
@@ -3,25 +3,34 @@ from rest_framework import serializers
 
 
 from accounts.api.serializers import UserDisplaySerializer
-from tweets.models import Tweet 
+from tweets.models import Tweet # from ..models import Tweet
+
 
 class TweetModelSerializer(serializers.ModelSerializer):
-	user = UserDisplaySerializer(read_only=True)
-	date_display = serializers.SerializerMethodField()
-	timesince = serializers.SerializerMethodField()
+    user = UserDisplaySerializer(read_only=True) #write_only
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
+    is_retweet = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Tweet
+        fields = [
+            'id',
+            'user',
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince',
+            'is_retweet',
+        ]
 
-	class Meta:
-		model = Tweet
-		fields = [
-			'user',
-			'content',
-			'timestamp',
-			'date_display',
-			'timesince',
-		]
+    def get_is_retweet(self, obj):
+        if obj.parent:
+            return True
+        return False
+    
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
 
-	def get_date_display(self, obj):
-		return obj.timestamp.strftime("%b %d, %Y | %I:%M %p")
-
-	def get_timesince(self, obj):
-		return timesince(obj.timestamp) + " ago"
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
--- tweetme/src/tweets/models.py
+++ tweetme/src/tweets/models.py
@@ -10,24 +10,43 @@ from .validators import validate_content
 
 # model manager
 
+class TweetManager(models.Manager):
+    def retweet(self, user, parent_obj):
+        if parent_obj.parent:
+            og_parent = parent_obj.parent
+        else:
+            og_parent = parent_obj
+        
+        obj = self.model(
+                parent = og_parent,
+                user = user,
+                content = parent_obj.content,
+            )
+        obj.save()
+        
+        return obj
+
+
 class Tweet(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.DO_NOTHING)
-	content   = models.CharField(max_length=140, validators=[validate_content])
-	updated   = models.DateTimeField(auto_now=True)
-	timestamp = models.DateTimeField(auto_now_add=True)
+    parent      = models.ForeignKey("self", blank=True, null=True)
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content     = models.CharField(max_length=140, validators=[validate_content])
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
-	
-	def __str__(self):
-		return str(self.content)
+    objects = TweetManager()
 
-	def get_absolute_url(self):
-		return reverse("tweet:detail", kwargs={"pk":self.pk})
+    def __str__(self):
+        return str(self.content)
 
-	class Meta:
-		ordering = ['-timestamp']
+    def get_absolute_url(self):
+        return reverse("tweet:detail", kwargs={"pk":self.pk})
 
-	#def clean(self, *args, **kwargs):
-	#	content = self.content
-	#	if content == "abc":
-	#		raise ValidationError("Content cannot be ABC")
-	#		return super (Tweet, self).clean(*args, **kwargs)
+    class Meta:
+        ordering = ['-timestamp']
+
+    # def clean(self, *args, **kwargs):
+    #     content = self.content
+    #     if content == "abc":
+    #         raise ValidationError("Content cannot be ABC")
+    #     return super(Tweet, self).clean(*args, **kwargs)
