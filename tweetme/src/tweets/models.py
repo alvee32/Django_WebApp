153fab51f239b3bccf2257544f04ed9ff7bf32af
esha-mahid <esha.mahid@yahoo.com>
2018-12-27 02:40:14 +0600

    Extended Length for User Tweet

--- tweetme/src/tweets/models.py
+++ tweetme/src/tweets/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 from .validators import validate_content
 
-
+# Define class for Tweet
 class Tweet(models.Model):
 	user      = models.ForeignKey(settings.AUTH_USER_MODEL)
-	content   = models.CharField(max_length=140, validators=[validate_content])
+	content   = models.CharField(max_length=150, validators=[validate_content])
 	updated   = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -20,7 +20,6 @@ class Tweet(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("tweet:detail", kwargs={"pk":self.pk})
-
 	#def clean(self, *args, **kwargs):
 	#	content = self.content
 	#	if content == "abc":
